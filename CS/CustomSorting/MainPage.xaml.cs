a81b02caf397d7cbaa382b736a3cf15ccc0fed18
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ CS/CustomSorting/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DevExpress.XtraGrid;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CustomSorting {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent();
+            grid.ItemsSource = new MonthList();
+            grid.SortBy(grid.Columns[0]);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            grid.Columns[0].SortMode = ColumnSortMode.Custom;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            grid.Columns[0].SortMode = ColumnSortMode.Default;
+        }
+
+        private void grid_CustomColumnSort(object sender, DevExpress.UI.Xaml.Grid.CustomColumnSortEventArgs e) {
+            e.Result = Comparer<int>.Default.Compare(e.ListSourceRowIndex1, e.ListSourceRowIndex2);
+
+            e.Handled = true;
+        }
+    }
+    public class Month {
+        public string month { get; set; }
+    }
+    public class MonthList : ObservableCollection<Month> {
+        public MonthList()
+            : base() {
+            Add(new Month() { month = "January" });
+            Add(new Month() { month = "February" });
+            Add(new Month() { month = "March" });
+            Add(new Month() { month = "April" });
+            Add(new Month() { month = "May" });
+            Add(new Month() { month = "June" });
+            Add(new Month() { month = "July" });
+            Add(new Month() { month = "August" });
+            Add(new Month() { month = "September" });
+            Add(new Month() { month = "October" });
+            Add(new Month() { month = "November" });
+            Add(new Month() { month = "December" });
+        }
+    }
+}
---
+++ VB/CustomSorting/MainPage.xaml.vb
@@ -0,0 +1,55 @@
+﻿Imports DevExpress.XtraGrid
+
+Public NotInheritable Class MainPage
+    Inherits Page
+    Public Sub New()
+        Me.InitializeComponent()
+        grid.ItemsSource = New MonthList()
+        grid.SortBy(grid.Columns(0))
+    End Sub
+
+    Private Sub CheckBox_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        grid.Columns(0).SortMode = ColumnSortMode.Custom
+    End Sub
+
+    Private Sub CheckBox_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        grid.Columns(0).SortMode = ColumnSortMode.Default
+    End Sub
+
+    Private Sub grid_CustomColumnSort(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Grid.CustomColumnSortEventArgs)
+        e.Result = Comparer(Of Integer).Default.Compare(e.ListSourceRowIndex1, e.ListSourceRowIndex2)
+
+        e.Handled = True
+    End Sub
+    Public Class Month
+        Private privatemonth As String
+        Public Property month() As String
+            Get
+                Return privatemonth
+            End Get
+            Set(ByVal value As String)
+                privatemonth = value
+            End Set
+        End Property
+    End Class
+    Public Class MonthList
+        Inherits ObservableCollection(Of Month)
+        Public Sub New()
+            MyBase.New()
+            Add(New Month() With {.month = "January"})
+            Add(New Month() With {.month = "February"})
+            Add(New Month() With {.month = "March"})
+            Add(New Month() With {.month = "April"})
+            Add(New Month() With {.month = "May"})
+            Add(New Month() With {.month = "June"})
+            Add(New Month() With {.month = "July"})
+            Add(New Month() With {.month = "August"})
+            Add(New Month() With {.month = "September"})
+            Add(New Month() With {.month = "October"})
+            Add(New Month() With {.month = "November"})
+            Add(New Month() With {.month = "December"})
+        End Sub
+    End Class
+End Class
+
+
